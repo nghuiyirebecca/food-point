8ea68985ac8d7702e4d294e963d2f4c029335df6
xenzil <xenzil@hotmail.com>
2015-06-23 16:45:05 +0800

    Update food-point.py

--- food-point.py
+++ food-point.py
@@ -9,12 +9,30 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/template"))
 
 class MainPage(webapp2.RequestHandler):
-    """ Handler for the front page."""
+    #Handler for the front page.
 
     def get(self):
         template = jinja_environment.get_template('home.html')
         self.response.out.write(template.render())
 
+
+# Handler when logged in
+class MainPageUser(webapp2.RequestHandler):
+    # Front page for those logged in
+
+    def get(self):
+        user = users.get_current_user()
+        if user:  # signed in already
+            template_values = {
+                'user_mail': users.get_current_user().email(),
+                'logout': users.create_logout_url(self.request.host_url),
+            }
+            template = jinja_environment.get_template('home_user.html')
+            self.response.out.write(template.render(template_values))
+        else:
+            self.redirect(self.request.host_url)
+
+"""
 # Datastore definitions
 class Persons(ndb.Model):
     # Models a person. Key is the email.
@@ -30,7 +48,7 @@ class Items(ndb.Model):
     date = ndb.DateTimeProperty(auto_now_add=True)
 	
 class newfoodlocation(webapp2.RequestHandler):
-    """ Form for getting and displaying all the food locations user has entered before. """
+     #Form for getting and displaying all the food locations user has entered before. 
 
     def show(self):
         # Displays the page. Used by both get and post
@@ -88,6 +106,7 @@ class DeleteItem(webapp2.RequestHandler):
         item.delete()
         self.redirect('/newfoodlocation')
 
-
-app = webapp2.WSGIApplication([('/', MainPage)],
+"""
+app = webapp2.WSGIApplication([('/', MainPage),
+								('/login', MainPageUser)],
                               debug=True)
